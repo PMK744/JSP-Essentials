a9b4b55b04ad263ade5742ed6e9859ce023a48cf
PMK744 <69542280+PMK744@users.noreply.github.com>
2020-12-30 21:48:45 -0600

    Added /pay

---
+++ Essentials/src/commands/pay.js
@@ -0,0 +1,42 @@
+const Plugin = require('../base/BasePlugin');
+
+module.exports = class pay extends Plugin {
+    constructor(pluginData) {
+        super(pluginData);
+        const moduleName = "[§5economyAPI§r]"
+        let checkConfig = setInterval(() => {
+            if (this.config == undefined) {} else {
+                clearInterval(checkConfig);
+                if (this.economy.commands.pay != "enabled") return;
+                this.getApi().getServer().getCommandManager().registerClassCommand(
+                    {
+                        id: 'pmk:pay',
+                        description: 'Checks your balance',
+                        flags: 0,
+                        aliases: [],
+                        execute: async (sender, args) => {
+                            if (!sender.isPlayer()) return this.logger.info(moduleName + " §cThis command can't be used in console!");
+                            if (!args[0]) return sender.sendMessage("§cInvaild Syntax!§r\nExample: /pay [target: player] [amount]");
+                            let target = this.getApi().getServer().getPlayerByName(args[0].replace("-", " "))
+                            if (target == null) return sender.sendMessage("§cNo player was found");
+                            if (target.username.name == sender.username.name) return sender.sendMessage("§cYou can't pay yourself.");
+                            let amount = parseInt(args.join(" ").replace(`${args[0]} `, ""));
+                            if (amount <= 0) return sender.sendMessage("§cThe minimum you can pay is $1!");
+                            let balance = await this.getBalanceByName(sender.username.name);
+                            if (balance >= amount) {
+                                sender.sendMessage(`§7You have successfully paid §9${target.username.name}§7 §a$${amount}§7!`);
+                                target.sendMessage(`§9${sender.username.name}§7 has paid you §a$${amount}§7!`);
+                                this.updateBalanceByName("remove", sender.username.name, amount)
+                                this.updateBalanceByName("add", target.username.name, amount)
+                            } else {
+                                return sender.sendMessage(`§cYou don't have enough money to pay §9${target.username.name}§c §a$${amount}§c.`);
+                            }
+                        },
+                    },
+                    this.getApi().getServer()
+                );
+            }
+        });
+    }
+};
+
